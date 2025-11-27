a95eea8ba646b8c1859b890df6c26a87b9433dab
Tomoki <sktar7214@gmail.com>
2025-11-28 01:49:01 +0900

    feat:display operator and operand, clear display, modify html class
    add display number, display operator, clear display, modify html class name and css

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 'use strict'
 
-//display number
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+
 const numberButtons = document.querySelectorAll(".number");
-  const currentInput = numberButtons.forEach(function (numberButton) {
+const display = document.querySelector(".display");
+const operatorButtons = document.querySelectorAll(".operator");
+
+
+//display number
+const inputNumber = numberButtons.forEach(function (numberButton) {
   numberButton.addEventListener("click", function () {
-    const display = document.querySelector(".display");
     display.textContent += this.textContent;
   })
 });
 
+//display operator
+const inputOperator = operatorButtons.forEach(function (operatorButton) {
+  operatorButton.addEventListener("click", function () {
+    firstOperand = display.textContent;
+    operator = this.textContent;
+    display.textContent = firstOperand + " " + operator + " ";
+  })
+});
+
+//display clear
+const clearButton = document.querySelector(".clear");
+  clearButton.addEventListener("click", function () {
+    display.textContent = "";
+  });
+  
